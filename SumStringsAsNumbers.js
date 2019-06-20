0f0eb3c6afb43a3d8728d55aa494e183bb6156a3
WANGYUJIE <langyuyang@outlook.com>
2019-06-20 22:14:08 +0200

    add: comments

--- SumStringsAsNumbers.js
+++ SumStringsAsNumbers.js
@@ -1,6 +1,12 @@
 // sumStrings('1','2') // => ‘3'
+// 难点：
+//    1. 大数的加减
+//    2. 进位的处理
+//    3. 参数的处理
+
 const str2list = str => [...str]
 
+// 参数的处理，去掉没用的0，注意对长度的影响
 const rmHead0 = str => {
   const list = str2list(str)
   while (list[0] === '0') {
@@ -8,14 +14,14 @@ const rmHead0 = str => {
   }
   return list.join('')
 }
-
+// 进位的处理: 变量的维护与使用
 const add = (long, short, addOnNext) => {
   const resList = long.map((str) => {
     let resultNum = addOnNext + Number(str) + (Number(short[0]) || 0)
     
     if (short.length > 0) short.shift()
     
-    if (resultNum > 9) {
+    if (resultNum > 9) { // 维护
       addOnNext = 1
       resultNum = (resultNum + '').charAt(1)
     } else {
@@ -25,7 +31,7 @@ const add = (long, short, addOnNext) => {
     return resultNum + ''
   })
   
-  if (addOnNext || resList[resList.length - 1] === '0') resList.push('1')
+  if (addOnNext) resList.push('1') // 特：最开头的一位数
   
   return resList.reverse().join('')
 }
@@ -61,12 +67,12 @@ function sumStrings(a,b) {
   while (index < a.length || index < b.length || carry != 0) {
     var aDigit = index < a.length ? parseInt(a[index]) : 0;
     var bDigit = index < b.length ? parseInt(b[index]) : 0;
-    var digitSum = aDigit + bDigit + carry;
-    sumDigits.push((digitSum % 10).toString());
+    var digitSum = aDigit + bDigit + carry; // 进位的处理: 变量的维护与使用，用取余数更好
+    sumDigits.push((digitSum % 10).toString()); 
     carry = Math.floor(digitSum / 10);
     index++;
   }
   sumDigits.reverse();
-  while (sumDigits[0] == '0') sumDigits.shift();
+  while (sumDigits[0] == '0') sumDigits.shift(); // 处理0开头参数的影响
   return sumDigits.join('');
 }
