511916f501f007fbd90fe743c3ab622d8f9dfe22
WANGYUJIE <langyuyang@outlook.com>
2019-06-24 16:50:11 +0200

    add test cases

--- ValidateSudokuWithSizeNxN.js
+++ ValidateSudokuWithSizeNxN.js
@@ -113,3 +113,30 @@ const Sudoku = function (data) {
     }
   }
 }
+
+const goodSudoku = new Sudoku(
+  [
+    [1, 4, 2, 3],
+    [3, 2, 4, 1],
+
+    [4, 1, 3, 2],
+    [2, 3, 1, 4]
+  ]
+)
+const badSudoku = new Sudoku(
+  [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [2, 3, 1, 5, 6, 4, 8, 9, 7],
+    [3, 1, 2, 6, 4, 5, 9, 7, 8],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [5, 6, 4, 8, 9, 7, 2, 3, 1],
+    [6, 4, 5, 9, 7, 8, 3, 1, 2],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [8, 9, 7, 2, 3, 1, 5, 6, 4],
+    [9, 7, 8, 3, 1, 2, 6, 4, 5]
+  ]
+)
+
+console.log(goodSudoku.isValid()) // true
+console.log(badSudoku.isValid()) // false: Sudoku with invalid boxes (little squares), but valid rows and columns
+
