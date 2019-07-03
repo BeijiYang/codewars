794caaa51c7afd8e657042e9abeedbd7047c08bd
BeijiYang <langyuyang@outlook.com>
2019-07-03 19:40:54 +0200

    add primorial of a number

---
+++ PrimorialOfANumber.js
@@ -0,0 +1,49 @@
+/*
+Definition (Primorial Of a Number)
+Is similar to factorial of a number, In primorial, not all the natural numbers get multiplied, only prime numbers are multiplied to calculate the primorial of a number. It's denoted with P#.
+
+Task
+Given a number N , calculate its primorial. !alt  !alt
+
+Notes
+Only positive numbers will be passed (N > 0) .
+Input >> Output Examples:
+1- numPrimorial (3) ==> return (30)
+Explanation:
+Since the passed number is (3) ,Then the primorial should obtained by multiplying 2 * 3 * 5 = 30 .
+
+Mathematically written as , P3# = 30 .
+2- numPrimorial (5) ==> return (2310)
+Explanation:
+Since the passed number is (5) ,Then the primorial should obtained by multiplying 2 * 3 * 5 * 7 * 11 = 2310 .
+
+Mathematically written as , P5# = 2310 .
+3- numPrimorial (6) ==> return (30030)
+Explanation:
+Since the passed number is (6) ,Then the primorial should obtained by multiplying 2 * 3 * 5 * 7 * 11 * 13 = 30030 .
+
+Mathematically written as , P6# = 30030 .
+Playing with Numbers Series
+Playing With Lists/Arrays Series
+For More Enjoyable Katas
+ALL translations are welcomed
+Enjoy Learning !!
+*/
+
+
+// solution 1
+const tempPrimeNumList = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+
+const numPrimorial = n => {
+  let result = 1
+  while (n > 0) {
+    result *= tempPrimeNumList[n - 1]
+    n -= 1
+  }
+  return result
+}
+
+
+console.log(
+  numPrimorial(3)
+)
