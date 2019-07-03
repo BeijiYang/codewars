7f245bf617eef65bd19dbf53dbda2299958d6f4e
BeijiYang <langyuyang@outlook.com>
2019-07-03 18:26:13 +0200

    add: special number

---
+++ SpecialNumber.js
@@ -0,0 +1,56 @@
+/*
+Definition
+A number is a Special Number *if it’s digits only consist 0, 1, 2, 3, 4 or 5 *
+
+Task
+Given a number determine if it special number or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+The number passed will be positive (N > 0) .
+
+All single-digit numbers with in the interval [0:5] are considered as special number.
+
+Input >> Output Examples
+1-  specialNumber(2) ==> return "Special!!"
+Explanation:
+It's a single-digit number within the interval [0:5] .
+
+2-  specialNumber(9) ==> return "NOT!!"
+Explanation:
+Although ,it's a single-digit number but Outside the interval [0:5] .
+
+3- specialNumber(23) ==> return "Special!!"
+Explanation:
+All the number's digits formed from the interval [0:5] digits .
+
+4- specialNumber(39) ==> return "NOT!!"
+Explanation:
+Although , there is a digit (3) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+
+5- specialNumber(59) ==> return "NOT!!"
+Explanation:
+Although , there is a digit (5) Within the interval But the second digit is not (Must be ALL The Number's Digits ) .
+
+6- specialNumber(513) ==> return "Special!!"
+7- specialNumber(709) ==> return "NOT!!"
+For More Enjoyable Katas
+ALL translation are welcomed
+Enjoy Learning !!
+*/
+
+const specialNumber = num =>
+  [...num.toString()].map(Number).every(n => n >= 0 && n <= 5)
+    ? "Special!!"
+    : "NOT!!"
+
+console.log(
+  specialNumber(2), // ==> return "Special!!"
+  specialNumber(9), // ==> return "NOT!!"
+  specialNumber(23), // ==> return "Special!!"
+  specialNumber(39), // ==> return "NOT!!"
+  specialNumber(59), // ==> return "NOT!!"
+  specialNumber(513), // ==> return "Special!!"
+  specialNumber(709), // ==> return "NOT!!"
+)
