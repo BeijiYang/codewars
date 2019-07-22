bec22989b8624d289b913d4c21551d028b1dcd0d
BeijiYang <langyuyang@outlook.com>
2019-07-22 11:18:10 +0200

    add len() for linked list

---
+++ 7kyu/lenList.js
@@ -0,0 +1,24 @@
+/**
+Implement the method length, which accepts a linked list (head), and returns the length of the list.
+
+For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+
+The linked list is defined as follows:
+
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+Note: the list may be null and can hold any type of value.
+
+Good luck!
+ */
+
+const length = head => {
+  let len = 0
+  while (head) {
+    len += 1
+    head = head.next
+  }
+  return len
+}
