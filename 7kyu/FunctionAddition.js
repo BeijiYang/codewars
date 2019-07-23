243b2210184f994149bab9db3aee1bac4b3fdc9e
BeijiYang <langyuyang@outlook.com>
2019-07-23 16:55:38 +0200

    add a ES6 => solution

---
+++ 7kyu/FunctionAddition.js
@@ -0,0 +1,13 @@
+/**
+ * Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+add_one = add(1)
+add_one(3)  # 4
+
+add_three = add(3)
+add_three(3) # 6
+ */
+
+const add = n => m => n + m
