8fb046e2741ce50145630e789a70b91f59783404
BeijiYang <langyuyang@outlook.com>
2019-07-23 17:17:02 +0200

    add partition func

---
+++ 5kyu/PartitionOn.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ Description:
+Write a function which partitions a list of items based on a given predicate.
+
+After the partition function is run, the list should be of the form [ F, F, F, T, T, T ] where the Fs (resp. Ts) are items for which the predicate function returned false (resp. true).
+
+NOTE: the partitioning should be stable; in other words: the ordering of the Fs (resp. Ts) should be preserved relative to each other.
+
+For convenience and utility, the partition function should return the boundary index. In other words: the index of the first T value in items.
+
+For example:
+
+var items = [1, 2, 3, 4, 5, 6];
+function isEven(n) {return n % 2 == 0}
+var i = partitionOn(isEven, items);
+// items should now be [1, 3, 5, 2, 4, 6]
+// i     should now be 3
+ */
+
+const partitionOn = (pred, items) => {
+  const [ts, fs] = partition(pred, items)
+  const newItems = [...ts, ...fs]
+  changeItmes(items, newItems)
+  return ts.length
+}
+
+
+const partition = (pred, list) => list.reduce(
+  ([ts, fs], cur) => !pred(cur) ? [[...ts, cur], fs] : [ts, [...fs, cur]],
+  [[], []]
+)
+
+const changeItmes = (items, newItems) => {
+  for (let i = 0; i < items.length; i++) {
+    items[i] = newItems[i]
+  }
+}
