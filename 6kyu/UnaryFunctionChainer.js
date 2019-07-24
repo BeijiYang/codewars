bf998a74b6e79010fc2a8e98bd015e65d8fddbfa
BeijiYang <langyuyang@outlook.com>
2019-07-24 19:40:44 +0200

    add Unary function chainer

---
+++ 6kyu/UnaryFunctionChainer.js
@@ -0,0 +1,27 @@
+/**
+Your task is to write a higher order function for chaining together a list of unary functions. In other words, it should return a function that does a left fold on the given functions.
+
+chained([a,b,c,d])(input)
+Should yield the same result as
+
+d(c(b(a(input))))
+ */
+
+const chained = fns => arg => fns.reduce((acc, cur) => cur(acc), arg)
+
+/** test cases
+
+function f1(x){ return x*2 }
+function f2(x){ return x+2 }
+function f3(x){ return Math.pow(x,2) }
+
+function f4(x){ return x.split("").concat().reverse().join("").split(" ")}
+function f5(xs){ return xs.concat().reverse() }
+function f6(xs){ return xs.join("_") }
+
+Test.assertEquals( chained([f1,f2,f3])(0), 4 )
+Test.assertEquals( chained([f1,f2,f3])(2), 36 )
+Test.assertEquals( chained([f3,f2,f1])(2), 12 )
+
+Test.assertEquals(chained([f4,f5,f6])("lorem ipsum"), "merol_muspi")
+ */
